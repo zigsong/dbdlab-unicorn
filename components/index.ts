4a115061c2d12c550d2d312d9ef1e63ec19d58ea
PotatoBerry <wlsdud40@gmail.com>
2020-04-02 01:10:36 +0900

    Remove unused files

--- components/index.ts
+++
@@ -1,9 +0,0 @@
-import UnicornButton from "./UnicornButton"
-import UnicornRadio from "./UnicornRadio"
-import UnicornTitle from "./UnicornTitle"
-import UnicornText from "./UnicornText"
-import UnicornEmptyRow from "./UnicornEmptyRow"
-
-export {
-  UnicornButton, UnicornRadio, UnicornTitle, UnicornText, UnicornEmptyRow
-}
