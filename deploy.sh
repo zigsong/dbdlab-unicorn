92f429dea0b6e19cfd57a00953663df11d01c186
PotatoBerry <wlsdud40@gmail.com>
2020-04-24 15:07:50 +0900

    Update home urls cont'd

--- deploy.sh
+++ deploy.sh
@@ -35,15 +35,15 @@ sed -i '' -e 's/img_th_link1/img_th_kf_surfer/g' ./build/test_result/7.html
 sed -i '' -e 's/img_th_link1/img_th_kf_clay/g' ./build/test_result/8.html
 sed -i '' -e 's/img_th_link1/img_th_kf_judge/g' ./build/test_result/9.html
 
-sed -i '' -e 's/img_th_link1/img_th_kf_wizard/g' ./build/test_result/wd.html
-sed -i '' -e 's/img_th_link1/img_th_kf_chameleon/g' ./build/test_result/ch.html
-sed -i '' -e 's/img_th_link1/img_th_kf_choongsin/g' ./build/test_result/cs.html
-sed -i '' -e 's/img_th_link1/img_th_kf_star/g' ./build/test_result/st.html
-sed -i '' -e 's/img_th_link1/img_th_kf_transparent/g' ./build/test_result/tr.html
-sed -i '' -e 's/img_th_link1/img_th_kf_inventor/g' ./build/test_result/in.html
-sed -i '' -e 's/img_th_link1/img_th_kf_surfer/g' ./build/test_result/su.html
-sed -i '' -e 's/img_th_link1/img_th_kf_clay/g' ./build/test_result/cl.html
-sed -i '' -e 's/img_th_link1/img_th_kf_judge/g' ./build/test_result/ju.html
+sed -i '' -e 's/img_th_link1/img_th_kf_wizard/g' ./build/wd.html
+sed -i '' -e 's/img_th_link1/img_th_kf_chameleon/g' ./build/ch.html
+sed -i '' -e 's/img_th_link1/img_th_kf_choongsin/g' ./build/cs.html
+sed -i '' -e 's/img_th_link1/img_th_kf_star/g' ./build/st.html
+sed -i '' -e 's/img_th_link1/img_th_kf_transparent/g' ./build/tr.html
+sed -i '' -e 's/img_th_link1/img_th_kf_inventor/g' ./build/in.html
+sed -i '' -e 's/img_th_link1/img_th_kf_surfer/g' ./build/su.html
+sed -i '' -e 's/img_th_link1/img_th_kf_clay/g' ./build/cl.html
+sed -i '' -e 's/img_th_link1/img_th_kf_judge/g' ./build/ju.html
 
 aws s3 sync ./build/ s3://dbd-unicorn
 aws cloudfront create-invalidation --distribution-id E1H3RXQY26KFNY --paths "/*"
