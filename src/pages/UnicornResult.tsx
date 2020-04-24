84d679a57d8836720779627e7c64d98ea30baa8e
PotatoBerry <wlsdud40@gmail.com>
2020-04-24 12:56:46 +0900

    카카오 공유 이미지 수정

--- src/pages/UnicornResult.tsx
+++ src/pages/UnicornResult.tsx
@@ -340,7 +340,7 @@ function UnicornResult(props: RouteChildrenProps) {
         content : {
           title: "후니콘 ‘우리는 어떤 유니콘일까’",
           description:"우리 제품/서비스의 사용자 경험 유형을 검사하고 우리가 어떤 유니콘인지 알아보세요.",
-          imageUrl: "https://https://whonicorn.dbdlab.io/thumbnail/img_th_link2.png",
+          imageUrl: `https://whonicorn.dbdlab.io/${unicornSquareImgs[unicornIndex]}`,
           link : {
             webUrl:"https://whonicorn.dbdlab.io",
             mobileWebUrl:"https://whonicorn.dbdlab.io"
